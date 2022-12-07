9951698e0c426082cec7121b8ae6d6ccb836b6a2
adam <adam@AdamMacBook-Pro.local>
2022-12-07 14:24:27 +0800

    fix: BABEL error: Nuxt.js 引入的 JS 大於500KB

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,6 +98,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      compact: false
+    },
     postcss: null,
     transpile: [
       'three'
